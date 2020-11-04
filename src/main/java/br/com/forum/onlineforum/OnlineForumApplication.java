e3762a0e5b25e82ef1309496cc3ec2c0d9da383e
Rodolfo Peratello <rodolfoperatello@hotmail.com>
2020-11-03 21:32:46 -0300

    Project Structure

---
+++ src/main/java/br/com/forum/onlineforum/OnlineForumApplication.java
@@ -0,0 +1,13 @@
+package br.com.forum.onlineforum;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OnlineForumApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OnlineForumApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/forum/onlineforum/OnlineForumApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.forum.onlineforum;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OnlineForumApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
